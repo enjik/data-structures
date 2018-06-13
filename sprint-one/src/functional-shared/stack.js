5a8c50dd1aa0cbbe62bf2bad2e5de8767a93e8f9
Enji <enjikim17@gmail.com>
2018-06-13 15:25:35 -0700

    functional-shared: stack passes all specs

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
+//object to hold methods for stack
 var stackMethods = {};
 
+//add push method to stackMethods
+stackMethods.push = function(value) {
+  //increase size
+  this.count++;
+  //add value as last key
+  var lastKey = (this.count).toString();
+  this.storage[lastKey] = value;
+}
 
+//add pop mehtod
+stackMethods.pop = function() {
+  //if stack is empty, return
+  if (this.count === 0) {
+    return ('stack is empty');
+  }
+  //store last value in stack
+  var lastKey = this.count.toString()
+  var temp = this.storage[lastKey];
+  //remove last value from stack
+  delete this.storage[lastKey]
+  //decrease size
+  this.count--;
+  //return last value
+  return temp;
+}
+
+//add size method
+stackMethods.size = function() {
+  return this.count;
+}
