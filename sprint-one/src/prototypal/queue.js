db7fd21887f4f493e060c1e46100e9aacebda996
Enji <enjikim17@gmail.com>
2018-06-13 15:42:45 -0700

    prototypal: queue passes specs

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,42 @@
+//prototypal inheritance
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.count = 0
+  someInstance.storage = {};
+  return someInstance;
 };
 
 var queueMethods = {};
 
-
+//add enqueue method
+queueMethods.enqueue = function(value) {
+  //shift all values to make room for value at first key
+  for (var key in this.storage) {
+    var nextKey = parseInt(key) + 1;
+    this.storage[nextKey] = this.storage[key];
+  }
+  //add value at first key
+  this.storage['1'] = value;
+  //increase size of queue
+  this.count++;
+}
+//add dequeue method
+queueMethods.dequeue = function() {
+  //if queue is empty, return
+  if (this.count === 0) {
+    return ('queue is empty');
+  }
+  //store value at last key
+  var lastKey = this.count.toString();
+  var temp = this.storage[lastKey];
+  //delete last value from queue
+  delete this.storage[lastKey];
+  //decrease size of queue
+  this.count--;
+  //return value at last key
+  return temp;
+}
+//add size method
+queueMethods.size = function() {
+  return this.count;
+}
