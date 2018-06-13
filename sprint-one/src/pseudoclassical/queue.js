af003b6e3d302ffc9ad5a954bd77867dcebc6b75
Enji <enjikim17@gmail.com>
2018-06-13 16:06:32 -0700

    pseudoclassical inheritance: queue and stack pass all specs

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,44 @@
+//psuedoclassical inheritance example
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //create object to represent queue
+  this.someInstance = {};
+  this.count = 0;
+  this.storage = {};
 };
 
+//add enqueue method
+Queue.prototype.enqueue = function (value) {
+  //shift the entire queue to make room for new value at beginning
+  for (var key in this.storage) {
+    var keyNum = parseInt(key) + 1;
+    this.storage[keyNum.toString()] = this.storage[key];
+  }
+  //add value to start of queue
+  this.storage['1'] = value;
 
+  //increases size of queue
+  this.count++;
+
+}
+
+//add dequeue method
+Queue.prototype.dequeue = function() {
+  //if queue is empty, return
+  if (this.count === 0) {
+    return ('queue is empty');
+  }
+  //store value at end of queue
+  var lastKey = this.count.toString();
+  var temp = this.storage[lastKey];
+  //remove value from end of queue
+  delete this.storage[lastKey];
+  //decrease size of queue
+  this.count--;
+  //return value at end of queue;
+  return temp;
+}
+
+//add size method
+Queue.prototype.size = function() {
+  return this.count;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,37 @@
+//pseudoclassical inheritance
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.someInstance = {};
+  this.count = 0;
+  this.storage = {};
 };
 
+//add push method to stackMethods
+Stack.prototype.push = function(value) {
+  //increase size
+  this.count++;
+  //add value as last key
+  var lastKey = (this.count).toString();
+  this.storage[lastKey] = value;
+}
 
+//add pop mehtod
+Stack.prototype.pop = function() {
+  //if stack is empty, return
+  if (this.count === 0) {
+    return ('stack is empty');
+  }
+  //store last value in stack
+  var lastKey = this.count.toString()
+  var temp = this.storage[lastKey];
+  //remove last value from stack
+  delete this.storage[lastKey]
+  //decrease size
+  this.count--;
+  //return last value
+  return temp;
+}
+
+//add size method
+Stack.prototype.size = function() {
+  return this.count;
+}
