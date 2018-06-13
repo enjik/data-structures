231312ed49f55d24a318b26b5fc9c1e11d9d2abc
Enji <enjikim17@gmail.com>
2018-06-13 12:26:26 -0700

    functional: stack passes specs

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //create new last key to store value
+    var lastKey = (size + 1).toString();
+    storage[lastKey] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size === 0) { return 'empty queue'};
+    var lastKey = size.toString();
+    var temp = storage[lastKey];
+    delete storage[lastKey];
+    size--;
+    return temp;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
