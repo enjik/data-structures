7c7e235b81e9eaaef798ccb258f028d8f8769385
Enji <enjikim17@gmail.com>
2018-06-13 12:19:58 -0700

    functional: queue passes specs

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,43 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {
+    stored: {},
+    count: 0
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //shift all elements to next key to make room for new value
+    for (var key in someInstance.stored) {
+      var keyNum = parseInt(key) + 1;
+      someInstance.stored[keyNum.toString()] = someInstance.stored[key];
+    }
+    //set first key to enqueued value
+    someInstance.stored['1'] = value;
+    //increase queue size
+    someInstance.count++;
   };
 
   someInstance.dequeue = function() {
+    if (someInstance.count === 0) {
+      return ('empty queue');
+    }
+    //store last value
+    var lastKey = someInstance.count.toString();
+    var temp = someInstance.stored[lastKey];
+    //remove last key, value in queue
+    delete someInstance.stored[lastKey];
+    //decrement queue size
+    someInstance.count--;
+    //return last value
+    return temp;
   };
 
   someInstance.size = function() {
+    return someInstance.count;
   };
 
   return someInstance;
