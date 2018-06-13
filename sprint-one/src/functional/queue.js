965352546c880115bdd1d1d7cfa19a82758258fc
Enji <enjikim17@gmail.com>
2018-06-13 12:27:26 -0700

    functional: refactoring of queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,43 +1,41 @@
 var Queue = function() {
-  var someInstance = {
-    stored: {},
-    count: 0
-  };
+  var someInstance = {};
 
   // Use an object with numeric keys to store values
-
+  var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
     //shift all elements to next key to make room for new value
-    for (var key in someInstance.stored) {
+    for (var key in storage) {
       var keyNum = parseInt(key) + 1;
-      someInstance.stored[keyNum.toString()] = someInstance.stored[key];
+      storage[keyNum.toString()] = storage[key];
     }
     //set first key to enqueued value
-    someInstance.stored['1'] = value;
+    storage['1'] = value;
     //increase queue size
-    someInstance.count++;
+    size++;
   };
 
   someInstance.dequeue = function() {
-    if (someInstance.count === 0) {
+    if (size === 0) {
       return ('empty queue');
     }
     //store last value
-    var lastKey = someInstance.count.toString();
-    var temp = someInstance.stored[lastKey];
+    var lastKey = size.toString();
+    var temp = storage[lastKey];
     //remove last key, value in queue
-    delete someInstance.stored[lastKey];
+    delete storage[lastKey];
     //decrement queue size
-    someInstance.count--;
+    size --;
     //return last value
     return temp;
   };
 
   someInstance.size = function() {
-    return someInstance.count;
+    return size;
   };
 
   return someInstance;
