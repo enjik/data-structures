08d86b74e8388bea8020d143ffe11ac7b050a58f
Enji <enjikim17@gmail.com>
2018-06-13 16:20:01 -0700

    es6-queue passes all specs

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,37 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  enqueue(value) {
+    //shift all values to make room for new first value
+    for (var key in this.storage) {
+      var nextKeyNum = parseInt(key) + 1;
+      this.storage[nextKeyNum.toString()] = this.storage[key];
+    }
+    //set first value
+    this.storage['1'] = value;
+    //increment queue size
+    this.count++;
+  }
+  dequeue() {
+    //if queue is empty, return
+    if (this.count === 0) {
+      return ('queue is empty');
+    }
+    //store last value
+    var lastKey = this.count.toString();
+    var temp = this.storage[lastKey];
+    //remove last value
+    delete this.storage[lastKey];
+    //decrement queue size
+    this.count--;
+    //return last value
+    return temp;
+  }
+  size() {
+    return this.count;
   }
 
 }
